549849d5987cdd890ae1a5713fe7c84d1de8a552
Estebantj88 <estebantorress.006@gmail.com>
2026-08-20 21:34:56 -0600

    homework 1

---
+++ Evidencia.md
@@ -0,0 +1,42 @@
+# Evidencia de resultados
+
+1. Se comprueba la correcta funcionalidad de la verificación del número de identificación, se digita un número ya existente y se rechaza el registro del nuevo estudiante
+![Screenshot From 2026-08-20 20-28-27.png](:/6eb5de8647034d9197389194811d3603)
+
+
+
+
+2. En esta prueba, se digita una identificación única y se comprueba el mensaje de éxito, confirmando que el estudiante se ha registrado correctamente y por ende se ha almacenado en el localStorage
+
+
+![Screenshot From 2026-08-20 20-31-32.png](:/a7917371035f4578a7a56a498d1e6764)
+
+   
+
+4. Se ingresa a las herramientas de desarrollo para revisar los datos almacenados en el localStorage, efectivamente se puede observar como está almacenado tanto la clave como el arreglo en formato de texto en la memoria del navegador.
+
+
+![Screenshot From 2026-08-20 20-54-07.png](:/49fac4cb6b154488b664ecfa00e89e37)
+
+
+
+5. Se verifica los registros almacenados en la tabla de estudiantes y se procede a eliminar el estudiante con ID `502520323`[cite: 5].
+![Screenshot From 2026-08-20 20-52-52.png](:/0c6ed92815f34808ade2dbd905b315f0)
+
+
+
+6. Al eliminar el registro se verifica la tabla y además se comprueba la actualización del contador de estudiantes
+
+![Screenshot From 2026-08-20 20-32-37.png](:/773ea3adcec84303a8ed40d0a92e50fe)
+
+
+
+
+7. Se corrobora la eliminación volviendo a comprobar los datos almacenados en el localStorage para confirmar que el registro fue borrado
+![Screenshot From 2026-08-20 20-57-21.png](:/0ed7d1e504324523bd12c06fe54079c2)
+
+8. Se realiza la búsqueda por ID
+![Screenshot From 2026-08-20 21-03-15.png](:/88a1e1ee370f4ff5903e4b56117a49a7)
+
+9. Se realiza la búsqueda por nombre
+![Screenshot From 2026-08-20 21-05-09.png](:/f4b1a8f14c664d5cbd0a0f9d062108fa)
---
+++ app.js
@@ -0,0 +1,241 @@
+const LOCAL_STORAGE_KEY = "students"
+
+const getStudents = () => {
+    const students = localStorage.getItem(LOCAL_STORAGE_KEY)
+
+    if (students === null) {
+        return []
+    }
+
+    return JSON.parse(students)
+};
+
+
+const saveStudents = (students) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(students))
+}
+
+
+const saveStudent = (student) => {
+    const students = getStudents()
+    students.push(student)
+    saveStudents(students)
+}
+
+
+
+
+const registerStudent = () => {
+    const form = document.getElementById("student-form")
+    const students = getStudents()
+
+    if (!form) {
+        return
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        if (!form.checkValidity()) {
+            form.reportValidity()
+            return
+        }
+
+
+        const nameInput = document.getElementById("student-name")
+        const emailInput = document.getElementById("student-email")
+        const idInput = document.getElementById("student-id")
+        const ageInput = document.getElementById("student-age")
+        const careerInput = document.getElementById("student-career")
+
+
+
+        const student = {
+            name: nameInput.value.trim(),
+            email: emailInput.value.trim(),
+            id: idInput.value.trim(),
+            age: ageInput.value,
+            career: careerInput.value
+
+        }
+
+
+        if (students.some(s => s.id === student.id)) {
+            alert(student.name + " cant be registered, the given ID already exists")
+            return
+        }
+
+        saveStudent(student)
+        alert("Student " + student.name + " has been registered")
+        form.reset()
+        window.location.href = "index.html"
+
+
+
+
+    })
+
+}
+
+
+
+const displayStudents = () => {
+
+    const studentList = document.getElementById("student-list")
+    const students = getStudents()
+
+    if (!students) {
+        return
+    }
+
+    studentList.innerHTML = ""
+
+    students.forEach((student, index) => {
+        const trow = document.createElement("tr")
+
+        const nameField = document.createElement("td")
+        nameField.textContent = student.name
+
+        const emailField = document.createElement("td")
+        emailField.textContent = student.email
+
+        const idField = document.createElement("td")
+        idField.textContent = student.id
+
+        const careerField = document.createElement("td")
+        careerField.textContent = student.career
+
+
+        const ageField = document.createElement("td")
+        ageField.textContent = student.age
+
+        const acctionsField = document.createElement("td")
+
+        const deleteBtn = document.createElement("button")
+        deleteBtn.type = "button"
+        deleteBtn.textContent = "Delete"
+        deleteBtn.classList.add("btn", "btn-delete")
+        deleteBtn.addEventListener("click", () => {
+            deleteStudent(index)
+        })
+
+
+        acctionsField.appendChild(deleteBtn)
+        trow.appendChild(idField)
+        trow.appendChild(nameField)
+        trow.appendChild(emailField)
+        trow.appendChild(ageField)
+        trow.appendChild(careerField)
+        trow.appendChild(acctionsField)
+        studentList.appendChild(trow)
+
+
+        countStudents()
+
+
+    });
+
+
+}
+
+
+
+const deleteStudent = (index) => {
+    const students = getStudents()
+    students.splice(index, 1)
+    saveStudents(students)
+    displayStudents()
+    countStudents()
+
+
+}
+
+
+
+const searchStudent = () => {
+    const searchForm = document.getElementById("search-form")
+    const searchBar = document.getElementById("search-bar")
+
+    if (!searchBar) {
+        return
+    }
+
+
+    if (searchForm) {
+        searchForm.addEventListener("submit", (e) => {
+            e.preventDefault()
+        })
+    }
+
+    searchBar.addEventListener("input", (e) => {
+        const searchTerm = e.target.value.toLowerCase().trim()
+        const tableRows = document.querySelectorAll('#student-list tr')
+
+        tableRows.forEach((row) => {
+            const trowtxt = row.textContent.toLowerCase()
+            if (trowtxt.includes(searchTerm)) {
+                row.style.display = ''
+
+            } else {
+                row.style.display = 'none'
+            }
+
+
+
+
+        })
+
+        countStudents()
+    })
+
+}
+
+
+
+const countStudents = () => {
+    const studentCounter = document.getElementById("student-counter")
+    const studentList = document.getElementById("student-list")
+    if (!studentCounter) {
+        return
+    }
+
+    const tableRows = document.querySelectorAll('#student-list tr')
+    const visibleRows = Array.from(tableRows).filter(row => row.style.display !== 'none')
+    studentCounter.textContent = visibleRows.length
+
+
+    const emptyRow = document.getElementById("empty-row")
+    if (visibleRows.length === 0) {
+
+        if (!emptyRow) {
+            const emptyRow = document.createElement("tr")
+            emptyRow.id = "empty-row"
+            const emptyField = document.createElement("td")
+
+            emptyField.textContent = "No results found"
+            emptyField.colSpan = 6
+            emptyField.style.textAlign = "center"
+            emptyRow.appendChild(emptyField)
+            studentList.appendChild(emptyRow)
+
+        }
+    } else {
+        if (emptyRow) {
+            emptyRow.remove()
+        }
+
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    registerStudent()
+    displayStudents()
+    searchStudent()
+    countStudents()
+})
+
+
+
+
+
